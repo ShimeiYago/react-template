4557f09e88eed517d158780fadc6909ca03d16bf
ShimeiYago <harrisonhightower4@gmail.com>
2022-01-26 00:54:56 +0900

    Change store structure

--- react-template/src/store/counter/counterSlice.spec.ts
+++ react-template/src/store/counter/counterSlice.spec.ts
@@ -2,7 +2,7 @@ import counterReducer, {
   increment,
   decrement,
   incrementByAmount,
-} from './slice';
+} from './counterSlice';
 import { CounterState } from './model';
 
 describe('counter reducer', () => {
